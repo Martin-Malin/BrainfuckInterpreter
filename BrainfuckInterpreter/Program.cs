a45508be155e5ec9c42412f0b37c795190c133fa
Adrien Caillol <acaillol@sii.fr>
2023-04-18 14:28:40 +0200

    Première implémentation "simple" (non testée)

--- BrainfuckInterpreter/Program.cs
+++ BrainfuckInterpreter/Program.cs
@@ -1,17 +1,92 @@
-﻿using BrainfuckInterpreter;
+﻿using System.Text;
 
 internal class Program
 {
+    private static byte[] data = new byte[30000];
+    private static int pointer = 0;
+
     private static void Main(string[] args)
     {
         string[] commands = args[0].Split("");
 
-        DataArray dataArray = new DataArray();
-        List<Command> commandes = new List<Command>();
-
+        List<int> loopIndexes = new List<int>();
         for (int i = 0; i < commands.Length; i++)
         {
-            commandes.Add(new Command(dataArray, commands[i]));
+            switch (commands[i])
+            {
+                case "+":
+                    IncreaseData();
+                    break;
+                case "-":
+                    DecreaseData();
+                    break;
+                case "<":
+                    PreviousByte();
+                    break;
+                case ">":
+                    NextByte();
+                    break;
+                case ".":
+                    byte byteOutput = GetValue();
+                    string consoleOutput = Encoding.ASCII.GetString(new byte[] { byteOutput });
+                    Console.Write(consoleOutput);
+                    break;
+                case ",":
+                    int key = Console.Read();
+                    byte[] byteInput = Encoding.ASCII.GetBytes(Convert.ToChar(key).ToString());
+                    SetValue(byteInput[0]);
+                    break;
+                case "[":
+                    if (data[pointer] == 0)
+                    {
+                        loopIndexes.Add(pointer);
+                        do
+                        { i++; } while (commands[i] != "]");
+                    }
+                    break;
+                case "]":
+                    if (data[pointer] != 0)
+                    {
+                        i = loopIndexes.Last();
+                    }
+                    break;
+            }
         }
+    }
+
+    public static void NextByte()
+    {
+        if (pointer == data.Length)
+            pointer = 0;
+        else
+            ++pointer;
+    }
+
+    public static void PreviousByte()
+    {
+        if (pointer == 0)
+            pointer = 30000;
+        else
+            --pointer;
+    }
+
+    public static void IncreaseData()
+    {
+        ++data[pointer];
+    }
+
+    public static void DecreaseData()
+    {
+        --data[pointer];
+    }
+
+    public static byte GetValue()
+    {
+        return data[pointer];
+    }
+
+    public static void SetValue(byte value)
+    {
+        data[pointer] = value;
     }
 }
